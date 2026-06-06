16480425a49c49d5139b4ea59f2b5169b7a07bb9
cauan745 <cauanramos745@gmail.com>
2026-06-06 17:48:07 -0400

    fix login page and add redirect in middleware

--- cmd/consumer/server/auth_middleware.go
+++ cmd/consumer/server/auth_middleware.go
@@ -6,15 +6,26 @@ import (
 
 func (s HttpServer) jwtMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Set("Expires", "0")
+
 		cookie, err := r.Cookie("session_token")
 		if err != nil {
-			http.Error(w, "token not found", 400)
+			//http.Error(w, "token not found", 400)
+			http.Redirect(w, r, "/login.html", http.StatusSeeOther)
+			return
+		}
+
+		if cookie.Value == "" {
+			http.Redirect(w, r, "/login.html", http.StatusSeeOther)
 			return
 		}
 
 		err = s.jwtMaker.VerifyToken(cookie.Value)
 		if err != nil {
-			http.Error(w, "invalid token", 400)
+			//http.Error(w, "invalid token", 400)
+			http.Redirect(w, r, "/login.html", http.StatusSeeOther)
 			return
 		}
 
